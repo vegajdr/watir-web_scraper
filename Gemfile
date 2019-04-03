820b7108d6c248548bd95ce28f07a1b367e4a093
vegajdr <vegajdr@users.noreply.github.com>
2019-04-03 16:46:06 -0400

    Initial Commit
    `gem bundle web_scraper` + `rubocop --auto-correct`

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in web_scraper.gemspec
+gemspec
---
+++ lib/web_scraper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'web_scraper/version'
+
+module WebScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
